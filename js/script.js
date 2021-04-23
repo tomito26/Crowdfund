195a46fad569e0e71183ae2e6edf782b7257dc52
tomito26 <tommybwah@gmail.com>
2021-04-23 16:17:23 +0300

    made the navbar reponsive in small devices

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let modalBtn = document.querySelector('.btn');
 let modalBg = document.querySelector('.modal-bg');
 let modalClose = document.querySelector('.fas');
+let noReward = document.querySelector('.no-reward');
+let burger = document.querySelector('.burger');
+let navLinks = document.querySelector('.nav-links');
+
+
+burger.addEventListener('click',()=>{
+    navLinks.classList.toggle('nav-active'); 
+    burger.classList.toggle('toggle')
+});
 
 modalBtn.addEventListener('click',()=>{
     modalBg.classList.add('bg-active');
@@ -9,3 +18,18 @@ modalBtn.addEventListener('click',()=>{
 modalClose.addEventListener('click',()=>{
     modalBg.classList.remove('bg-active');
 });
+
+
+noReward.addEventListener('click',()=>{
+    let firstPledge = document.querySelector('.pledge1');
+    if(!noReward.checked){
+        firstPledge.style.border = '2px solid hsl(0, 0%, 90%)';
+        console.log('its checked');
+    }else{
+        firstPledge.style.border = '2px solid hsl(176, 50%, 47%)';
+    };
+   
+});
+
+
+
